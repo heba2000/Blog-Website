f7bf170e79e262eb0b7ffd1859a810df1ba94f3a
heba2000 <53901861+heba2000@users.noreply.github.com>
2022-01-12 14:33:21 +0200

    Delete Back End - NodeJS directory

--- Back End - NodeJS/filePackage.js
+++
@@ -1,6 +0,0 @@
-const fs = require ('fs');
-const readMyFile = (urlPath)=>JSON.parse(fs.readFileSync(urlPath,{encoding:'utf-8'}));
-const writeMyFile = (urlPath,data)=> (fs.writeFileSync(urlPath,JSON.stringify(data)));
-
-//to export functions
-module.exports = {readMyFile , writeMyFile};
